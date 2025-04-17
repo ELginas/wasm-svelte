c1e464297b5511350b810bc6eba3c68c162bcc49
ELginas <gintaras.z123@yahoo.com>
2025-04-17 16:56:36 +0300

    basic remark

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-  documentation
-</p>
+<script>
+  import { unified } from "unified";
+  import remarkParse from "remark-parse";
+  import remarkRehype from "remark-rehype";
+  import rehypeStringify from "rehype-stringify";
+
+  let text = $state("# Title\nthis is the text");
+  let file = $state(null);
+
+  $effect(() => {
+    (async () => {
+      file = await unified()
+        .use(remarkParse)
+        .use(remarkRehype)
+        .use(rehypeStringify)
+        .process(text);
+    })();
+  });
+</script>
+
+<div class="h-full flex flex-col items-center justify-center">
+  <div class="flex gap-2 h-full my-5">
+    <textarea class="" bind:value={text}></textarea>
+    <div class="flex flex-col gap-2">
+      {#if file}
+        <pre>{file.value}</pre>
+      {/if}
+    </div>
+    {#if file}
+      <div class="">{@html file.value}</div>
+    {/if}
+  </div>
+</div>
